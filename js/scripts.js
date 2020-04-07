2378ac6d1ea5541edc36dc451b7c2743a537bead
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-07 16:24:11 -0700

    add methods to calculate and show ticket cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,41 @@
+// Business Logic -----------------------
 function Ticket(agePrice, moviePrice, showTimePrice){
-  this.agePrice = agePrice;
-  this.moviePrice = moviePrice;
-  this.showTimePrice = showTimePrice;
+  this.agePrice = agePrice,
+  this.moviePrice = moviePrice,
+  this.showTimePrice = showTimePrice,
+  this.basePrice = 7
 }
+
+
+Ticket.prototype.printTicket = function(totalPrice){
+  
+  $("p#hidden").show();
+  $("#cost").html(totalPrice);
+
+
+
+};
+
+Ticket.prototype.addPrices = function() {
+  var totalPrice = this.agePrice + this.moviePrice + this.showTimePrice + this.basePrice;
+  console.log("Add prices called, total price is: ", totalPrice);
+  this.printTicket(totalPrice);
+};
+
+
+// user interface logic -------------------------------
 
 $(document).ready(function() {
   $("form").submit(function(event){
     event.preventDefault();
 
-    var agePrice = $("#age").val();
-    var moviePrice = $("#movie").val();
-    var showTimePrice = $("#showtime").val();
-    new Ticket = new Ticket (agePrice, moviePrice, showtimePrice);
-  })
-})
+    var agePrice = parseInt($("#age").val());
+    var moviePrice = parseInt($("#movie").val());
+    var showTimePrice = parseInt($("#showtime").val());
+    var newTicket = new Ticket (agePrice, moviePrice, showTimePrice);
+    
+    newTicket.addPrices();
+    // newTicket.printTicket(totalPrice);
+    
+  });
+});
